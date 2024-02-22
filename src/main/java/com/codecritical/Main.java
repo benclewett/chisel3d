80fa9423e504ea62ad110ddc358080e522ec21d4
ben <ben.clewett@gmail.com>
2024-02-22 22:15:50 +0000

    Missing Image

--- src/main/java/com/codecritical/Main.java
+++ src/main/java/com/codecritical/Main.java
@@ -15,12 +15,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(String.format("%.10f", -0.7385747949549769-0.000000067290415));
-        System.out.println(String.format("%.10f", -0.7385747949549769+0.000000067290415));
-        System.out.println(String.format("%.10f", 0.17536094397069943-0.000000067290415));
-        System.out.println(String.format("%.10f", 0.17536094397069943+0.000000067290415));
-
-
         if (args.length == 0) {
             throw new RuntimeException("Please pass the model type as the only argument: " + Arrays.asList(EModel.values()));
         }
